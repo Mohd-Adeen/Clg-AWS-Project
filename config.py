02832f490c47a38f34cfa03e6c264f9aa16d430f
thekabirselot <kabirselot@eng.rizvi.edu.in>
2024-06-03 03:37:27 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-3.c9icmcae892c.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "database-3"
+custombucket = "bucketforproject3"
+customregion = "us-east-1"
