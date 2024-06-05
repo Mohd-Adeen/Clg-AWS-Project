88394170ccf3e996189eb333120405731ebe379b
thekabirselot <kabirselot@eng.rizvi.edu.in>
2024-06-05 09:30:33 +0530

    Initial Commit

--- EmpApp.py
+++ EmpApp.py
@@ -4,31 +4,32 @@ import os
 import boto3
 from config import *
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 bucket = custombucket
 region = customregion
 
 db_conn = connections.Connection(
-    host = customhost,
-    port = 3306,
-    user = customuser,
-    password = custompass,
-    db = customdb
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
 )
+
 output = {}
-table = 'employee';
+table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html');
+    return render_template('AddEmp.html')
 
-@app.route("/about", methods=['POST'])
+@app.route("/about", methods=['GET'])
 def about():
-    return render_template('www.intellipaat.com');
+    return render_template('about.html')
 
 @app.route("/addemp", methods=['POST'])
-def AddEmp():
+def add_emp():
     emp_id = request.form['emp_id']
     first_name = request.form['first_name']
     last_name = request.form['last_name']
@@ -36,45 +37,46 @@ def AddEmp():
     location = request.form['location']
     emp_image_file = request.files['emp_image_file']
 
-    insert_sql = "INSERT INTO employee VALUES(%s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO employee (emp_id, first_name, last_name, pri_skill, location) VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
-    
+
     if emp_image_file.filename == "":
         return "Please select a file"
     
     try:
         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
         db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-        #Upload image file in S3
-        emp_image_file_name_in_s3 = "emp-id-"+str(emp_id)+"_image_file"
+        emp_name = first_name + " " + last_name
+
+        # Upload image file to S3
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
             s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
+            s3_location = bucket_location['LocationConstraint']
 
             if s3_location is None:
                 s3_location = ''
             else:
                 s3_location = '-' + s3_location
-            
+
             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
                 s3_location,
                 custombucket,
-                emp_image_file_name_in_s3 
+                emp_image_file_name_in_s3
             )
 
         except Exception as e:
             return str(e)
-    
+
     finally:
         cursor.close()
 
     print("All Modification Done...")
     return render_template('AddEmpOutput.html', name=emp_name)
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(host='0.0.0.0', port=80, debug=True)
